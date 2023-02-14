d6005927afacd3ce72e3c43ebb46b94207fe3920
Nking217 <38316404+Nking217@users.noreply.github.com>
2023-02-14 17:59:56 +0200

    Adding Firebase

--- app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/LoginScreen.java
+++ app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/LoginScreen.java
@@ -3,12 +3,28 @@ package com.example.vr_iot_shooting_simulator_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class LoginScreen extends AppCompatActivity {
+    private EditText editTextEmail, editTextPassword;
+    private Button signIN;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+
+        signIN = (Button) FindViewById(R.id.signIn);
+        signIN.SetOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById((R.id.password);
     }
 }
--- app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/MainActivity.java
+++ app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.vr_iot_shooting_simulator_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+            }, 3000);
+
+
     }
+
 }
