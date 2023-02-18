989a5febde7e6128ae0865d0043f7cbcd31d607d
Valifso <harryy5up@gmail.com>
2023-02-18 22:12:16 +0200

    Starting To Add Firestore

--- app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/HomeActivity.java
+++ app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/HomeActivity.java
@@ -3,13 +3,44 @@ package com.example.vr_iot_shooting_simulator_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
+    private EditText Username;
+    private EditText Coins;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        db = FirebaseFirestore.getInstance();
     }
+
+
+    db.collection("users")
+            Username.get();
+        Username.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+                }
+            } else {
+                Log.w(TAG, "Error getting documents.", task.getException());
+            }
+        }
+    });
+
 
 }
