00c78411073397ab80945792e064905deb4247be
Valifso <harryy5up@gmail.com>
2023-04-17 21:49:39 +0300

    bottom nav
    operational bottom nav view
    start working on the home fragment

--- app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/HomeActivity.java
+++ app/src/main/java/com/example/vr_iot_shooting_simulator_android_app/HomeActivity.java
@@ -4,6 +4,9 @@ import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +14,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.vr_iot_shooting_simulator_android_app.databinding.ActivityHomeBinding;
+import com.example.vr_iot_shooting_simulator_android_app.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -25,6 +30,7 @@ import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
     TextView textFullName, textCoins;
     String rtvFullName;
     long rtvCoins;
@@ -37,9 +43,34 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
         textFullName = findViewById(R.id.textView6);
         textCoins = findViewById(R.id.Coins);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.history:
+                    replaceFragment(new HistoryFragment());
+                    break;
+                case R.id.statistics:
+                    replaceFragment(new StatisticsFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -81,6 +112,13 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
 
 
 
